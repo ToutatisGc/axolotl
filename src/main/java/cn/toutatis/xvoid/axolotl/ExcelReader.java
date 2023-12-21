ab3db1ad59addc23d9a66ffc47635e7ab3ec2e7c
Toutatis_Gc <gc@toutatis.cn>
2023-12-21 22:20:20 +0800

    INIT COMMIT

---
+++ src/main/java/cn/toutatis/xvoid/axolotl/ExcelReader.java
@@ -0,0 +1,11 @@
+package cn.toutatis.xvoid.axolotl;
+
+import cn.toutatis.xvoid.toolkit.log.LoggerToolkit;
+import org.slf4j.Logger;
+
+public class ExcelReader {
+
+    private static final Logger LOGGER  = LoggerToolkit.getLogger(ExcelReader.class);
+
+
+}
---
+++ src/test/java/cn/toutatis/xvoid/axolotl/ReadExcelTest.java
@@ -0,0 +1,12 @@
+package cn.toutatis.xvoid.axolotl;
+
+import org.junit.Test;
+
+public class ReadExcelTest {
+
+    @Test
+    public void readExcel(){
+
+    }
+
+}
