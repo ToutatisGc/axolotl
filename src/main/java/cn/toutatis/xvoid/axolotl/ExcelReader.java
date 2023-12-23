a5fed2687e8db890144549147bee4cb5dff67f65
Toutatis_Gc <gc@toutatis.cn>
2023-12-23 11:48:46 +0800

    补充监测结果

--- src/main/java/cn/toutatis/xvoid/axolotl/ExcelReader.java
+++ src/main/java/cn/toutatis/xvoid/axolotl/ExcelReader.java
@@ -1,7 +1,8 @@
 package cn.toutatis.xvoid.axolotl;
 
+import cn.toutatis.xvoid.axolotl.support.DetectResult;
+import cn.toutatis.xvoid.axolotl.support.TikaShell;
 import cn.toutatis.xvoid.toolkit.log.LoggerToolkit;
-import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
 import org.slf4j.Logger;
 
 import java.io.File;
@@ -11,6 +12,7 @@ public class ExcelReader {
     private static final Logger LOGGER  = LoggerToolkit.getLogger(ExcelReader.class);
 
     public ExcelReader(File excelFile) {
-
+        DetectResult detect = TikaShell.detect(excelFile, TikaShell.OOXML_EXCEL);
+//        if (detect.isDetect())
     }
 }
--- src/main/java/cn/toutatis/xvoid/axolotl/support/DetectResult.java
+++ src/main/java/cn/toutatis/xvoid/axolotl/support/DetectResult.java
@@ -3,6 +3,9 @@ package cn.toutatis.xvoid.axolotl.support;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.tika.mime.MimeType;
+
+import java.util.Objects;
 
 /**
  * 文件检测结果
@@ -14,12 +17,35 @@ public class DetectResult {
     /**
      * 是否识别通过
      */
-    private boolean detect = false;
+    private Boolean detect;
 
     /**
      * 文件检测结果
      */
     private String message;
 
+    /**
+     * 识别到的文件类型
+     */
+    private MimeType catchMimeType;
+
+    /**
+     * 期望的文件类型
+     */
+    private MimeType wantedMimeType;
+
     public DetectResult(boolean detect) {this.detect = detect;}
+
+    public DetectResult(boolean detect, String message) {
+        this.detect = detect;
+        this.message = message;
+    }
+
+    public Boolean isDetect() {
+        return Objects.requireNonNullElseGet(detect, () -> wantedMimeType.equals(catchMimeType));
+    }
+
+    public Boolean isWantedMimeType() {
+        return catchMimeType.equals(wantedMimeType);
+    }
 }
