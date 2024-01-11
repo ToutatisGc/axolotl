d5a2e9515a3b9fbcd1bb6368283859332594ced6
Toutatis_Gc <gc@toutatis.cn>
2024-01-11 15:54:46 +0800

    降低validator分支

--- src/test/java/cn/toutatis/xvoid/axolotl/entities/ValidTestEntity.java
+++ src/test/java/cn/toutatis/xvoid/axolotl/entities/ValidTestEntity.java
@@ -1,8 +1,10 @@
 package cn.toutatis.xvoid.axolotl.entities;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 
 @Data
