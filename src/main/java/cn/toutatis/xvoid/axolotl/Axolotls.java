0b84e455170963022dddb8270308931c66fccf7c
Toutatis_Gc <gc@toutatis.cn>
2024-03-09 11:48:42 +0800

    语法糖更新

--- src/main/java/cn/toutatis/xvoid/axolotl/Axolotls.java
+++ src/main/java/cn/toutatis/xvoid/axolotl/Axolotls.java
@@ -3,9 +3,13 @@ package cn.toutatis.xvoid.axolotl;
 import cn.toutatis.xvoid.axolotl.excel.reader.AxolotlExcelReader;
 import cn.toutatis.xvoid.axolotl.excel.reader.AxolotlStreamExcelReader;
 import cn.toutatis.xvoid.axolotl.excel.writer.*;
+import cn.toutatis.xvoid.axolotl.excel.writer.support.AxolotlWriteResult;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 文档加载器静态构造
@@ -70,6 +74,37 @@ public class Axolotls {
      */
     public static AxolotlTemplateExcelWriter getTemplateExcelWriter(File template, TemplateWriteConfig writeConfig){
         return new AxolotlTemplateExcelWriter(template, writeConfig);
+    }
+
+    /**
+     * 获取模板Excel写入器
+     * 默认写入第0张表，采用默认策略管理
+     * @param template 模板文件
+     * @param outputStream 输出流
+     * @return Excel写入器
+     */
+    public static AxolotlTemplateExcelWriter getTemplateExcelWriter(File template, OutputStream outputStream){
+        TemplateWriteConfig templateWriteConfig = new TemplateWriteConfig();
+        templateWriteConfig.setOutputStream(outputStream);
+        return getTemplateExcelWriter(template, templateWriteConfig);
+    }
+
+    /**
+     * [语法糖]
+     * 直接调取模板写入器写入数据
+     * @param template 模板文件
+     * @param outputStream 输出流
+     * @param fixMapping 引用字段
+     * @param datas 列表数据
+     * @return 写入结果
+     */
+    public static AxolotlWriteResult templateWrite(File template, OutputStream outputStream, Map<String,?> fixMapping, List<?> datas){
+        TemplateWriteConfig templateWriteConfig = new TemplateWriteConfig();
+        templateWriteConfig.setOutputStream(outputStream);
+        AxolotlTemplateExcelWriter templateExcelWriter = getTemplateExcelWriter(template, templateWriteConfig);
+        AxolotlWriteResult result = templateExcelWriter.write(fixMapping, datas);
+        templateExcelWriter.close();
+        return result;
     }
 
     /**
