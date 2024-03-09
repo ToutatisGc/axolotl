7baf3bb4a2215b58475e6c852d5458299efa6cea
Toutatis_Gc <gc@toutatis.cn>
2024-03-09 10:37:42 +0800

    更新计算问题

--- src/main/java/cn/toutatis/xvoid/axolotl/excel/writer/AutoWriteConfig.java
+++ src/main/java/cn/toutatis/xvoid/axolotl/excel/writer/AutoWriteConfig.java
@@ -1,8 +1,11 @@
 package cn.toutatis.xvoid.axolotl.excel.writer;
 
+import cn.toutatis.xvoid.axolotl.excel.writer.components.SheetTitle;
+import cn.toutatis.xvoid.axolotl.excel.writer.exceptions.AxolotlWriteException;
 import cn.toutatis.xvoid.axolotl.excel.writer.style.ExcelStyleRender;
 import cn.toutatis.xvoid.axolotl.excel.writer.support.CommonWriteConfig;
 import cn.toutatis.xvoid.axolotl.excel.writer.themes.ExcelWriteThemes;
+import cn.toutatis.xvoid.toolkit.validator.Validator;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
@@ -15,10 +18,39 @@ import lombok.SneakyThrows;
 @EqualsAndHashCode(callSuper = true)
 public class AutoWriteConfig extends CommonWriteConfig {
 
+    public AutoWriteConfig() {
+        super(true);
+    }
+
+    public AutoWriteConfig(Class<?> metaClass,boolean withDefaultConfig) {
+        super(withDefaultConfig);
+        this.metaClass = metaClass;
+        if(metaClass != null){
+            SheetTitle sheetTitle = metaClass.getDeclaredAnnotation(SheetTitle.class);
+            if(sheetTitle != null){
+                this.title = sheetTitle.value();
+                String sheetName = sheetTitle.sheetName();
+                if (Validator.strNotBlank(sheetName)){
+                    this.sheetName = sheetName;
+                }else{
+                    this.sheetName = this.title;
+                }
+            }
+        }else {
+            throw new AxolotlWriteException("元信息Class为空");
+        }
+
+    }
+
     /**
      * 标题
      */
     private String title;
+
+    /**
+     * 元数据类
+     */
+    private Class<?> metaClass;
 
     /**
      * 工作表名称
