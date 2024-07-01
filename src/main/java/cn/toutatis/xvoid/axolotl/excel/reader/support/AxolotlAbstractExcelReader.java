022dfc805e261e2a8488f97fc49dc35530c6122d
Toutatis_Gc <gc@toutatis.cn>
2024-07-01 17:50:24 +0800

    移除表头空格

--- src/main/java/cn/toutatis/xvoid/axolotl/excel/reader/support/AxolotlAbstractExcelReader.java
+++ src/main/java/cn/toutatis/xvoid/axolotl/excel/reader/support/AxolotlAbstractExcelReader.java
@@ -405,6 +405,7 @@ public abstract class AxolotlAbstractExcelReader<T> {
                             Cell cell = cellIterator.next();
                             // 应该没有人会用数字当表头吧...
                             if (cell != null && cell.getCellType() == CellType.STRING){
+                                // FIX 20240701 移除两边空格
                                 String cellValue = cell.getStringCellValue();
                                 if (headerKeys.containsKey(cellValue) && notAlreadyRecordKeys.containsKey(cellValue)){
                                     LoggerHelper.debug(LOGGER,LoggerHelper.format("查找到表头[%s]", cellValue));
